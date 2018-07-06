3a76f8b4b7b3a2253beb049b21ab0ca1d8618eb7
Kadex <>
2018-07-06 13:17:07 -0600

    Combat-Sandbox & Cameras
    +New scene for testing player movement and camera view.
    +Camera that follows player
    +Playable character via standard unity asset pack

---
+++ DragonRPG/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    GameObject player; 
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Cam()
+    {
+        transform.position = player.transform.position;
+    }
+	void LateUpdate () {
+        Cam();
+	}
+}
